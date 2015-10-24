beed198830369043b65e3d2134c2e6f9b6a5eeea
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-24 12:57:59 -1000

    geolocation origin set successfully

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -1,7 +1,5 @@
 //any js functions we want to run on the map go here
 Template.map.rendered = function() {
-  var lat;
-  var lon;
   function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -10,18 +8,23 @@ Template.map.rendered = function() {
     }
   }
   function showPosition(position) {
-    return [position.coords.latitude, position.coords.longitude];
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var origin = [lat, lon];
+
+    // hard coded orig
+    // var origin = [21.2968065, -157.8565356];
+
+    var map = L.map('map').setView(origin, 17);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution : '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var originMarker = L.marker(origin).addTo(map);
+    return [lat, lon];
   }
 
-  // var origin = getLocation();
-  // console.log(origin);
-  var origin = [21.2968065, -157.8565356];
+  getLocation();
 
-  var map = L.map('map').setView(origin, 17);
-
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution : '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors'
-  }).addTo(map);
-
-  var originMarker = L.marker(origin).addTo(map);
 };
