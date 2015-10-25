54b6249132613ba56f1d61be4af5e62f2c1d10ac
mikey1977 <mmi@umich.edu>
2015-10-25 06:23:29 -1000

    waypoint dates added, working on sorting

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -1,4 +1,6 @@
 //any js functions we want to run on the map go here
+
+
 Template.map.rendered = function() {
   function getLocation() {
     if (navigator.geolocation) {
@@ -16,17 +18,20 @@ Template.map.rendered = function() {
     // var origin = [21.2968065, -157.8565356];
 
     var map = L.map('map').setView(origin, 17);
-
+    var latest = null;
     map.on('click', function (e) {
         var waypoint = [e.latlng.lat, e.latlng.lng];
         L.marker(waypoint).addTo(map);
+        console.log("1", waypoint);
 
-        Waypoints.insert({
+
+        latest = Waypoints.insert({
             destination: waypoint,
             index: Waypoints.find().count(),
+            date: new Date()
         })
     });
-    
+
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution : '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors'
     }).addTo(map);
@@ -38,18 +43,94 @@ Template.map.rendered = function() {
         destinationMarker.setLatLng(event.latlng);
       } else {
         destinationMarker = L.marker(event.latlng).addTo(map);
+        // console.log("4", destinationMarker._latlng.lat, destinationMarker._latlng.lng);
       }
-    });
+    })
+    $('#pin').on('click', function(){
+      // console.log("4", destinationMarker._latlng.lat, destinationMarker._latlng.lng);
+      var waypoints = Waypoints.findOne({_id: latest });
+      // var waypoints = Waypoints.find({"_id" : {"$lt" : date}})
+      // var waypoints = Waypoints.find().sort({date: 1});
+      // var waypoints = Waypoints.find({ $first : "date"});
+      // var waypoints = Waypoints.find({ _id : 1 });
+      console.log("", waypoints);
+    })
+    // console.log(destinationMarker._latlng.lat, destinationMarker._latlng.lng);
+
+    // map.on('click', function(event){
+    //   Waypoints.findOne(destinationMarker)
+    // })
     return [lat, lon];
   }
 
   getLocation();
 
-  L.getMap('map').on('click', onMapClick);
+  // L.getMap('map').on('click', onMapClick);
 
-  function onMapClick(e) {
-      var waypoint = [e.latitude, e.longitude];
-      L.marker(waypoint).addTo(L.map('map'));
-  }
+  // function onMapClick(e) {
+  //     var waypoint = [e.latitude, e.longitude];
+  //     L.marker(waypoint).addTo(L.map('map'));
+  // }
 
 };
+
+// Template.map.events({
+//   'click #pin' : function(e,t){
+//     e.preventDefault();
+//     // console.log(destinationMarker._latlng.lat, destinationMarker._latlng.lng);
+//     console.log(t);
+//     // onMapClick(e);
+
+//   }
+// });
+
+// function onMapClick(e) {
+
+//   L.marker(waypoint).addTo(L.map('map'));
+
+// }
+
+// var saved = [];
+
+// function showPosition(position) {
+//   var lat = position.coords.latitude;
+//   var lon = position.coords.longitude;
+//   var origin = [lat, lon];
+
+//   // hard coded orig
+//   // var origin = [21.2968065, -157.8565356];
+
+//   var map = L.map('map').setView(origin, 17);
+
+//   map.on('click', function (e) {
+//       var waypoint = [e.latlng.lat, e.latlng.lng];
+//       L.marker(waypoint).addTo(map);
+//       console.log("1", waypoint);
+
+//       Waypoints.insert({
+//           destination: waypoint,
+//           index: Waypoints.find().count(),
+//       })
+//   });
+
+//   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution : '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors'
+//   }).addTo(map);
+
+//   var originMarker = L.marker(origin).addTo(map);
+//   var destinationMarker = null;
+//   map.on('click', function(event) {
+//     if (destinationMarker) {
+//       destinationMarker.setLatLng(event.latlng);
+//     } else {
+//       destinationMarker = L.marker(event.latlng).addTo(map);
+//     }
+//   })
+//   // console.log(destinationMarker._latlng.lat, destinationMarker._latlng.lng);
+
+//   // map.on('click', function(event){
+//   //   Waypoints.findOne(destinationMarker)
+//   // })
+//   return [lat, lon];
+// }
+
